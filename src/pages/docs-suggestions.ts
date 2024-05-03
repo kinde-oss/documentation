14923930c130bfc4d4b8c85b089149415b6d8daf
Marcos Martini <marcos@kinde.com>
2024-05-03 11:42:18 +1000

    testing glob

---
+++ src/pages/docs-suggestions.ts
@@ -0,0 +1,18 @@
+import type {APIRoute} from "astro";
+
+export const GET: APIRoute = () => {
+  const docs = import.meta.glob("../content/docs/**/**/*.mdx", {eager: true}) as any;
+
+  return new Response(
+    JSON.stringify(
+      Object.keys(docs)
+        // .filter((url) => docs[url].frontmatter.app_context)
+        .map((url) => ({
+          url: "https://docs.kinde.com" + url.split("/content/docs")[1].split(".")[0] + "/",
+          ...docs[url].frontmatter
+        })),
+      null,
+      2
+    )
+  );
+};
