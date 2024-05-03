2743e0ae8279407d37e0ffb3995713b3d17c625c
Marcos Martini <marcos@kinde.com>
2024-05-03 12:11:59 +1000

    return all docs

--- src/pages/docs-suggestions.json.ts
+++ src/pages/docs-suggestions.json.ts
@@ -5,13 +5,11 @@ export const GET: APIRoute = () => {
 
   return new Response(
     JSON.stringify(
-      Object.keys(docs)
-        .filter((url) => docs[url].frontmatter.app_context)
-        .map((url) => ({
-          url: "https://docs.kinde.com" + url.split("/content/docs")[1].split(".")[0] + "/",
-          title: docs[url].frontmatter.title,
-          app_context: docs[url].frontmatter.app_context
-        })),
+      Object.keys(docs).map((url) => ({
+        url: "https://docs.kinde.com" + url.split("/content/docs")[1].split(".")[0] + "/",
+        title: docs[url].frontmatter.title,
+        app_context: docs[url].frontmatter.app_context
+      })),
       null,
       2
     )
