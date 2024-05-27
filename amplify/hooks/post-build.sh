0fc67b36be4bb44ef0a5a1ce1d26a970ff98c300
Marcos Martini <marcosmartini7@gmail.com>
2024-05-27 10:24:46 +1000

    reverted invalidation tests

--- amplify/hooks/post-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Define the distribution ID and paths
-DISTRIBUTION_ID="EQ2XW1BRKG0SE"
-PATHS="/*"
-
-# Run the AWS CLI command to create an invalidation
-aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "$PATHS"
