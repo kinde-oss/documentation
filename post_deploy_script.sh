782902e36e769d558ec8523b58260bb2f93fb163
Marcos Martini <marcosmartini7@gmail.com>
2024-07-12 18:58:35 +1000

    removed post deploy script

--- post_deploy_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Deployment completed. Performing post-deployment actions..."
-
-# Invalidate CloudFront distribution
-DISTRIBUTION_ID="EQ2XW1BRKG0SE"
-aws cloudfront create-invalidation --distribution-id $DISTRIBUTION_ID --paths "/*"
-
-echo "Post-deployment actions completed."
