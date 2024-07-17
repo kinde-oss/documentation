bc3ea456134e2737bc95758026662af0a3fef3f2
Marcos Martini <marcosmartini7@gmail.com>
2024-07-17 10:33:52 +1000

    Refactor links and a few components, removed unused ones

---
+++ src/data/links.ts
@@ -0,0 +1,7 @@
+const discordLink = "https://discord.gg/wHX6j7wG5d";
+const roadmapLink = "https://updates.kinde.com/board";
+const slackLink =
+  "https://join.slack.com/t/thekindecommunity/shared_invite/zt-2k5i0aeet-d6Z_2qYphcNCpj0bFa4oCg";
+const supportLink = "mailto:support@kinde.com";
+
+export {discordLink, roadmapLink, slackLink, supportLink};
